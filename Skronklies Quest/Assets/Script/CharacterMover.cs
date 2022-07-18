39b2bff9f2c40583cd7bdf25be1f8bd667ff6402
Drac0BK <55963016+Drac0BK@users.noreply.github.com>
2022-07-18 15:55:41 +1000

    movement update

--- Skronklies Quest/Assets/Script/CharacterMover.cs
+++ Skronklies Quest/Assets/Script/CharacterMover.cs
@@ -5,15 +5,51 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     // Update is called once per frame
+    private float movementSpeed = 5f;
+    private float jumpVelocity = 10f;
+
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        characterMovement();
+
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        //transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, 0);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded() || Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
         {
-            transform.position = transform.position + new Vector3(-1,0,0);
+            rb.velocity = Vector2.up * jumpVelocity;
         }
-        if(Input.GetKeyDown(KeyCode.D))
+
+    }
+
+    private bool IsGrounded() { return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded; }
+
+    void characterMovement()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+        else
         {
-            transform.position = transform.position + new Vector3(1, 0, 0);
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.velocity = new Vector2(+movementSpeed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
         }
     }
+
 }
