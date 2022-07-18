f303b65485c925f4730ae7e8c22826253c88e834
Drac0BK <55963016+Drac0BK@users.noreply.github.com>
2022-07-18 16:52:10 +1000

    Pick up and drop

--- Skronklies Quest/Assets/Script/CharacterControls.cs
+++ Skronklies Quest/Assets/Script/CharacterControls.cs
@@ -8,6 +8,10 @@ public class CharacterControls : MonoBehaviour
     private float movementSpeed = 5f;
     private float jumpVelocity = 10f;
 
+    public Transform carrySpot;
+    bool carryObject = false;
+    GameObject carry = null;
+
     Rigidbody2D rb;
     private void Start()
     {
@@ -31,10 +35,18 @@ public class CharacterControls : MonoBehaviour
             rb.velocity = Vector2.up * jumpVelocity;
         }
 
+        if(CanPickUp() && Input.GetKeyDown(KeyCode.E) && carryObject == false)
+        {
+            PickUpObject();
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && carryObject == true)
+        {
+            DropObject();
+        }
     }
 
     private bool IsGrounded() { return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded; }
-
+    private bool CanPickUp() { return transform.Find("PickUpCheck").GetComponent<PickUpObjectCheck>().CanPickUp; }
     void characterMovement()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
@@ -50,6 +62,25 @@ public class CharacterControls : MonoBehaviour
                 rb.velocity = new Vector2(0, rb.velocity.y);
             }
         }
+    }
+
+    void PickUpObject()
+    {
+        carry = GameObject.Find("PickUpCheck").GetComponent<PickUpObjectCheck>().pickUpObject;
+        carry.transform.parent = transform;
+        carry.transform.position = carrySpot.transform.position;
+        Rigidbody2D rb = carry.GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        carryObject = true;
+    }
+
+    void DropObject()
+    {
+        carry.transform.parent = null;
+        carry.transform.position = transform.position + Vector3.right + Vector3.right;
+        carryObject = false;
+        Rigidbody2D rb = carry.GetComponent<Rigidbody2D>();
+        rb.isKinematic = false;
     }
 
 }
--- Skronklies Quest/Assets/Script/PickUpObjectCheck.cs
+++ Skronklies Quest/Assets/Script/PickUpObjectCheck.cs
@@ -7,14 +7,18 @@ public class PickUpObjectCheck : MonoBehaviour
     [SerializeField] private LayerMask platformLayerMask;
 
     public bool CanPickUp;
+    public GameObject pickUpObject;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        //Debug.Log(collision.gameObject.layer);
         CanPickUp = collision != null && (((1 << collision.gameObject.layer) & platformLayerMask) != 0);
+        pickUpObject = collision.gameObject;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         CanPickUp = false;
+        pickUpObject = null;
     }
 }
