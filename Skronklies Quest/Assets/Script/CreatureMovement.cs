6e51c011a2b4579d0bd89b55900ba6082dd8a990
Drac0BK <55963016+Drac0BK@users.noreply.github.com>
2022-07-19 10:25:53 +1000

    creature movement

--- Skronklies Quest/Assets/Script/CreatureMovement.cs
+++ Skronklies Quest/Assets/Script/CreatureMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class CreatureMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float timer = 1.0f;
+    int direction = 0;
+    float movementSpeed = 1.0f;
+    Rigidbody2D rb = null;
+
+    [SerializeField]
+    BoxCollider2D collider1 = null;
+    [SerializeField]
+    BoxCollider2D collider2 = null;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        direction = Random.Range(0, 2);
+        if (direction == 0)
+        {
+            collider1.enabled = false;
+        }
+        else if (direction == 1)
+        {
+            collider2.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(transform.parent == null)
+        {
+            if(direction == 0)
+            {
+                rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+                collider2.enabled = true;
+                collider1.enabled = false;
+            }
+            else if (direction == 1)
+            {
+                rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+                collider1.enabled = true;
+                collider2.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (direction == 0)
+        {
+            direction = 1;
+            Debug.Log("Hity");
+        }
+        else if (direction == 1)
+        {
+            direction = 0;
+            Debug.Log("hurty");
+        }
     }
 }
